d2a859d78666014622daee7138c88c6426d4ac46
Huu <phamhuu.a2.zip@gmail.com>
2019-08-14 21:08:03 +0700

    lesson1

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,7 @@
+float a = 7;
+float b = 3;
+float x;
+x = -b/a;
+
+system.out
+
---
+++ JavaApplication1/src/javaapplication1/aaaa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Huucock
+ */
+public class aaaa {
+
+    public static void main(String[] args) {
+        float a = 7;
+        float b = 3;
+        float x;
+        x = -b / a;
+        int sodu = 7%3;
+        
+
+
+    }
+}
